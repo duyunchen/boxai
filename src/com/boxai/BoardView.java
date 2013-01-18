7faf270b688d82a67ac80f10cef3f2f6f5f5d786
Jocelin Lee <contact@jocelinlee.com>
2013-01-18 00:29:48 -0500

    Fixed the board overflow problem and also fixed the orientation to portrait-only

--- src/com/boxai/BoardView.java
+++ src/com/boxai/BoardView.java
@@ -7,60 +7,74 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 public class BoardView extends View {
 
-	private static final int BOARD_WIDTH = 10;
-	private static final int BOARD_HEIGHT = 10;
+    private static final int BOARD_WIDTH = 10;
+    private static final int BOARD_HEIGHT = 10;
 
-	private final Paint mPaint = new Paint();
+    private final Paint mPaint = new Paint();
 
-	private Bitmap[] TILE_BITMAPS = new Bitmap[2];
+    private Bitmap[] TILE_BITMAPS = new Bitmap[2];
 
-	private ArrayList<Entity> entities = new ArrayList<Entity>();
+    private ArrayList<Entity> entities = new ArrayList<Entity>();
 
-	private final int TILE_SIZE = 70;
+    private int width, height;
 
-	public BoardView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		initBoardView(context);
+    private int TILE_SIZE = 70;
 
-		invalidate();
-	}
+    public BoardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initBoardView(context);
 
-	public BoardView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		initBoardView(context);
-	}
+        // get size of the window
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
 
-	@Override
-	public void onDraw(Canvas canvas) {
-		for (int ii = 0; ii < BOARD_WIDTH; ii++) {
-			for (int jj = 0; jj < BOARD_HEIGHT; jj++) {
-				canvas.drawBitmap(TILE_BITMAPS[1], ii * TILE_SIZE, jj
-						* TILE_SIZE, mPaint);
-			}
-		}
+        Point size = new Point();
+        display.getSize(size);
+        this.width = size.x;
+        this.height = size.y;
 
-	}
+        TILE_SIZE = this.width / BOARD_WIDTH;
 
-	private void initBoardView(Context context) {
-		Resources r = this.getResources();
+        invalidate();
+    }
 
-		this.loadTile(1, r.getDrawable(R.drawable.tile));
-	}
+    public BoardView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initBoardView(context);
+    }
 
-	public void loadTile(int key, Drawable tile) {
-		Bitmap bitmap = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE,
-				Bitmap.Config.ARGB_8888);
-		Canvas canvas = new Canvas(bitmap);
-		tile.setBounds(0, 0, TILE_SIZE, TILE_SIZE);
-		tile.draw(canvas);
+    @Override
+    public void onDraw(Canvas canvas) {
+        for (int ii = 0; ii < BOARD_WIDTH; ii++) {
+            for (int jj = 0; jj < BOARD_HEIGHT; jj++) {
+                canvas.drawBitmap(TILE_BITMAPS[1], ii * TILE_SIZE, jj * TILE_SIZE, mPaint);
+            }
+        }
 
-		TILE_BITMAPS[key] = bitmap;
-	}
+    }
+
+    private void initBoardView(Context context) {
+        Resources r = this.getResources();
+
+        this.loadTile(1, r.getDrawable(R.drawable.tile));
+    }
+
+    public void loadTile(int key, Drawable tile) {
+        Bitmap bitmap = Bitmap.createBitmap(TILE_SIZE, TILE_SIZE, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        tile.setBounds(0, 0, TILE_SIZE, TILE_SIZE);
+        tile.draw(canvas);
+
+        TILE_BITMAPS[key] = bitmap;
+    }
 
 }
